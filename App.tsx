86998369578591c7f7863cd6f5992ed66bd7be62
lucaophp-dinni <58432806+lucaophp-dinni@users.noreply.github.com>
2022-09-07 18:12:42 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Calculadora do Lucão</h1>
+      <p>Aqui tudo é foda!</p>
+    </div>
+  );
+}
